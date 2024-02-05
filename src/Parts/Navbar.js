ce2717ee835597d06dfaf4d6aae93d0f0dbadfb3
muhammadalvin92 <alvinbomber87@gmail.com>
2024-02-05 11:17:29 +0700

    [ UPDATE ] Navbar Menu Active

--- src/Parts/Navbar.js
+++ src/Parts/Navbar.js
@@ -1,42 +1,43 @@
 import '../Css/Navbar.css';
-
 import Navbar from 'react-bootstrap/Navbar';
+import Logout from '.././Parts/Logout'
+
 import Umkm from '../Images/logo-umkm.png';
 import Navbg from '../Images/bg-navbar.png';
 
 import { Icon } from '@iconify/react';
-
-import Logout from '.././Parts/Logout'
-
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 
 function Navigasi() {
   return (
-    <Navbar fixed="top"  className='Navbar '>
-        <Navbar.Brand className='nav-logo' href="#home">
-            <img className='Navbg' src={Navbg} alt="Navbg" />
-            <div className='nav-absolute'>
-              <div className='na-logo'>
-                <img className='Umkm' src={Umkm} alt="Umkm" />
-              </div>
-              <div className='na-menu d-flex gap-4'>
-                <Link className='' to={'/main'}><p className='navbar'>MAIN</p></Link>
-                <Link className='' to={'/umkm'}><p className='navbar'>UMKM</p></Link>
-                <Link className='' to={'/teknologi'}><p className='navbar'>TEKNOLOGI</p></Link>
-                <Link className='' to={'/profile'}><h5 className='navbar'><Icon icon="iconoir:profile-circle" /></h5></Link>
-              </div>
-            </div>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        {/* <Navbar.Collapse className="nav-collapse justify-content-end">
-          <Navbar.Text className='menu d-flex gap-3 py-3 px-5'>
-            <a href=''>HOME</a>
-            <a href=''>UMKM</a>
-            <a href=''>KUISIONER</a>
-            <a href=''>FASILITATOR</a>
-          </Navbar.Text>
-        </Navbar.Collapse> */}
-    </Navbar>
+    <Navbar fixed="top" className='Navbar '>
+  <Navbar.Brand className='nav-logo' href="#home">
+    <img className='Navbg' src={Navbg} alt="Navbg" />
+    <div className='nav-absolute'>
+      <div className='na-logo'>
+        <img className='Umkm' src={Umkm} alt="Umkm" />
+      </div>
+      <div className='na-menu d-flex gap-4'>
+        <NavLink className='' to={'/main'} activeClassName='nav-active' exact>
+          <p className='navbar'>MAIN</p>
+        </NavLink>
+        <NavLink className='' to={'/umkm'} activeClassName='nav-active'>
+          <p className='navbar'>UMKM</p>
+        </NavLink>
+        <NavLink className='' to={'/teknologi'} activeClassName='nav-active'>
+          <p className='navbar'>TEKNOLOGI</p>
+        </NavLink>
+        <NavLink className='' to={'/profile'} activeClassName='nav-active'>
+          <h5 className='navbar'><Icon icon="iconoir:profile-circle" /></h5>
+        </NavLink>
+      </div>
+    </div>
+  </Navbar.Brand>
+  <Navbar.Toggle />
+</Navbar>
+
   );
 }
 
